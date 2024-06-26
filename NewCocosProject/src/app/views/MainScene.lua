089e29f7e343c815cc739c753da2556843f56ad2
hayden <78656544987@gmail.com>
2024-06-26 20:28:55 +0800

    i

--- NewCocosProject/src/app/views/MainScene.lua
+++ NewCocosProject/src/app/views/MainScene.lua
@@ -3,8 +3,23 @@ local MainScene = class("MainScene", cc.load("mvc").ViewBase)
 
 MainScene.RESOURCE_FILENAME = "MainScene.csb"
 
+-- 获取UI控件
+MainScene.RESOURCE_BINDING = {
+    ["Btn_1"]   = {["varname"] = "btn"},
+    ["Check_1"] = {["varname"] = "check_1"},
+}
+
+
 function MainScene:onCreate()
     printf("resource node = %s", tostring(self:getResourceNode()))
+    print("====MainScene:onCreate","****")
+
+    -- 按钮使用
+    self.btn:addClickEventListener(function(sender)
+        print("====info","****")
+    end)
+ 
+    -- self.check_1:setVisible(false)
     
     --[[ you can create scene with following comment code instead of using csb file.
     -- add background image
