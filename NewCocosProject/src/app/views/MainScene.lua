c55360a7fbfa9b5bcde0c951664e0a59efb793d3
hayden <78656544987@gmail.com>
2024-06-28 17:31:50 +0800

    转动动画

--- NewCocosProject/src/app/views/MainScene.lua
+++ NewCocosProject/src/app/views/MainScene.lua
@@ -95,15 +95,26 @@ function MainScene:onCreate()
     
         -- -- 将目标位置转换为世界坐标系
         local endPosition = targetButton:getParent():convertToWorldSpace(cc.p(endPositionX,endPositionY))
-    
+        -- 添加随机偏移量
+        local offsetX = math.random(-100, 100)
+        local offsetY = math.random(-50, 50)
+        endPosition.x = endPosition.x + offsetX
+        endPosition.y = endPosition.y + offsetY
         -- 创建飞行动作
-        local moveAction = cc.MoveTo:create(0.3, endPosition)
+        local moveAction = cc.MoveTo:create(0.2, endPosition)
+        -- local rotateAction = cc.RotateBy:create(1.0, 360)
+        -- 创建逐渐停止的旋转动作
+        local rotateEaseOut = cc.EaseIn:create(cc.RotateBy:create(1, 2600), 0.5)
+
         local callback = cc.CallFunc:create(function()
             -- 动作完成后的回调，可以在这里添加其他逻辑
             print("===Chip reached target button")
             -- chip:removeFromParent()  -- 移除筹码精灵
         end)
-        local sequence = cc.Sequence:create(moveAction, callback)
+
+        -- 并行动作和顺序动作组合
+        local spawn = cc.Spawn:create(moveAction, rotateEaseOut)
+        local sequence = cc.Sequence:create(spawn, callback)
     
         -- 运行动作
         chip:runAction(sequence)
